a26e65869aebb541b2af59b6217c722b5426eb88
Роман Толмачёв <unifiq@gmail.com>
2021-11-26 03:20:52 +0700

    хранение и проверка pin-кода при входе. с оформлением - беда...

--- Gallery/Gallery/Pages/PinCodePage.xaml.cs
+++ Gallery/Gallery/Pages/PinCodePage.xaml.cs
@@ -1,5 +1,4 @@
-﻿
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Gallery.Pages
@@ -8,19 +7,58 @@ namespace Gallery.Pages
 	public partial class PinCodePage : ContentPage
 	{
 		private static string _pin;
-
+		private static bool _isExist = false;
+		private static int _attemptsCounter = 0;
 		public PinCodePage()
 		{
 			InitializeComponent();
 
-			if(App.Current.Properties.TryGetValue("pin", out object pin))
+			// Устанавливаем динамический ресурс с помощью специального метода
+			lblTitle.SetDynamicResource(Label.TextColorProperty, "growingColor");
+			lblTitle.SetDynamicResource(Label.FontSizeProperty, "growingFont");
+			Resources["growingFont"] = 15;
+			Resources["growingColor"] = Color.FromRgb(10, 10, 10);
+
+			if (App.Current.Properties.TryGetValue("pin", out object pin))
 			{
+				_isExist = true;
 				_pin = pin.ToString();
+				btnSave.Text = "Проверить";
 			}
 			else
 			{
 				entPIN.Placeholder = "введите свой первый pin";
+				btnSave.Text = "Сохранить";
 			}
+		}
+
+		private async void btnSave_Clicked(object sender, System.EventArgs e)
+		{
+			if (_isExist)
+			{
+				if (_pin == entPIN.Text)
+				{
+					_attemptsCounter++;
+				}
+				else
+				{
+					if (_attemptsCounter <= 5)
+					{
+						// Обновление динамического ресурса
+						Resources["growingFont"] = 15 + ++_attemptsCounter * 4;
+						Resources["growingColor"] = Color.FromRgb(10 + _attemptsCounter * 40, 10, 10);
+					}
+
+					return;
+				}
+			}
+			else
+			{
+				App.Current.Properties.Add("pin", entPIN.Text);
+				_isExist = true;
+			}
+
+			await Navigation.PushAsync(new MainPage());
 		}
 	}
 }
