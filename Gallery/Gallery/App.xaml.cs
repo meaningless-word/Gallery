7ecd817c03990e3c18e6c10627897a533577c925
Роман Толмачёв <unifiq@gmail.com>
2021-11-26 02:06:41 +0700

    Add project files.

---
+++ Gallery/Gallery/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Gallery.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gallery
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new NavigationPage(new PinCodePage());
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ Gallery/Gallery/Pages/PinCodePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gallery.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PinCodePage : ContentPage
+	{
+		private static string _pin;
+
+		public PinCodePage()
+		{
+			InitializeComponent();
+
+			if(App.Current.Properties.TryGetValue("pin", out object pin))
+			{
+				_pin = pin.ToString();
+			}
+			else
+			{
+				entPIN.Placeholder = "введите свой первый pin";
+			}
+		}
+	}
+}
